92adb3addc0f46447a6d5bdd9bb41cbb41af55c3
Ben Collins <aggieben@gmail.com>
2022-06-18 08:40:39 -0500

    experiment a bit with settings to see what works

--- plugins/logseq-bible-study/src/App.fs
+++ plugins/logseq-bible-study/src/App.fs
@@ -8,26 +8,36 @@ open Logseq.Settings
 
 let registerSettings() =
     [|
-        { key = "bible-api"
-          ``type`` = SettingSchemaType.Enum
-          ``default`` = (!^ [|"Biblia.com"|]) |> Some
-          title = "Bible API"
-          description = "API used to retrieve bible passages"
-          inputAs = None
-          enumChoices = [|"Biblia.com";"bible.org"|] |> Some
-          enumPicker = SettingSchemaEnumPickerType.Select |> Some }
+        // { key = "bible-api"
+        //   ``type`` = SettingSchemaType.Enum
+        //   ``default`` = (!^ [|"Biblia.com"|]) |> Some
+        //   title = "Bible API"
+        //   description = "API used to retrieve bible passages"
+        //   inputAs = None
+        //   enumChoices = [|"Biblia.com";"bible.org"|] |> Some
+        //   enumPicker = SettingSchemaEnumPickerType.Select |> Some }
 
-        { key = "Translation"
-          ``type`` = SettingSchemaType.Enum
-          ``default`` = (!^ [|"ESV"|]) |> Some
-          title = "Bible Translation"
-          description = "Translation of scripture to request"
+        // { key = "bible-translation"
+        //   ``type`` = SettingSchemaType.Enum
+        //   ``default`` = (!^ [|"ESV"|]) |> Some
+        //   title = "Bible Translation"
+        //   description = "Translation of scripture to request"
+        //   inputAs = None
+        //   enumChoices = [|"ESV";"KVJ";"LSB";"NIV"|] |> Some
+        //   enumPicker = SettingSchemaEnumPickerType.Select |> Some }
+        { key = "test"
+          ``type`` = SettingSchemaType.String
+          ``default`` = (!^ [|String.Empty|]) |> Some
+          title = "Test"
+          description = "Test Description"
           inputAs = None
-          enumChoices = [|"ESV";"KVJ";"LSB";"NIV"|] |> Some
-          enumPicker = SettingSchemaEnumPickerType.Select |> Some }
+          enumChoices = None
+          enumPicker = None }
     |] |> logseq.useSettingsSchema
 
 let main() =
     registerSettings() |> ignore
 
-logseq.ready(main).catch(fun o -> JS.console.error o; o) |> ignore
+logseq.ready(main)
+      .catch(fun o -> JS.console.error o; o) 
+|> ignore
